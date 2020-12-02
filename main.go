13456e3453ed322aac2cb15555acc2401dc389e8
Eric Auld <eric.auld@gmail.com>
2020-12-02 10:21:48 -0800

    Add an endpoint which serves the todo list in JSON
    Created a struct called TodoItem. Currently it just has a single field which is
    a string: the title of the item. Fixed a
    bug where if the name of this field did not start with a capital letter,
    it couldn't be encoded into JSON.

--- main.go
+++ main.go
@@ -5,14 +5,46 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
 )
+
+type todoItem struct {
+	Title string
+}
+
+var db *sql.DB
+
+func getTodoListEndpoint(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+
+	SQLQuery := "SELECT title FROM tasks;"
+	rows, err := db.Query(SQLQuery)
+	if err != nil {log.Fatal(err)}
+	defer rows.Close()
+
+	var todoItems []todoItem
+	for rows.Next() {
+		var item todoItem
+		err = rows.Scan(&item.Title)
+		if err != nil {log.Fatal(err)}
+		todoItems = append(todoItems, item)
+	}
+
+	json.NewEncoder(writer).Encode(todoItems)
+}
 
 func main() {
 	password, err := ioutil.ReadFile("./password.txt")
 	if err != nil {log.Fatal(err)}
 
-	_, err = sql.Open("mysql",
+	db, err = sql.Open("mysql",
 						fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/TodoList", password))
+	if err != nil {log.Fatal(err)}
+	defer db.Close()
+
+	http.HandleFunc("/", getTodoListEndpoint)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {log.Fatal(err)}
 }
