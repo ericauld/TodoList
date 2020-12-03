017ef801eaf6d2af1f00383209852174b15eb356
Eric Auld <eric.auld@gmail.com>
2020-12-03 15:25:52 -0800

    Add dummy "Delete item" buttons to each todo item
    Currently all the delete item buttons know how to do is to remember
    the title of the item they are next to, and send that title as an alert
    to the user.

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,35 +4,45 @@ import React from 'react';
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {items: [],
-                  inputBox: ''};
-                  
+    this.state = {
+      items: [],
+      inputBox: ''
+    };
+
     // this.createNewItem = this.createNewItem.bind(this);
     // this.handleChange = this.createNewItem.bind(this);
   }
-  
+
   createNewItem = (e) => {
     if (e.key === 'Enter') {
       var formData = new FormData()
       formData.append('Title', this.state.inputBox)
-      fetch("/api/newItem", {method: "POST", body: formData})
+      fetch("/api/newItem", { method: "POST", body: formData })
       this.setState((state, props) => ({
-        items: [...state.items, {"Title": this.state.inputBox}],
+        items: [...state.items, { "Title": this.state.inputBox }],
         inputBox: ''
       }))
     }
   }
 
-  handleChange = (e) => {this.setState({inputBox: e.target.value});}
+  deleteItem = (title) => {alert(title)}
+
+  handleChange = (e) => { this.setState({ inputBox: e.target.value }); }
 
   render() {
     return (
       <div>
-        <input value={this.state.inputBox} onChange={this.handleChange} onKeyPress={this.createNewItem }/>
+        <input
+          value={this.state.inputBox}
+          onChange={this.handleChange}
+          onKeyPress={this.createNewItem} />
         <ul>
           {this.state.items.map(item => (
             <li>
               {item.Title}
+              <button onClick={(e) => this.deleteItem(item.Title)}>
+                Delete
+              </button>
             </li>
           ))}
         </ul>
@@ -43,7 +53,7 @@ class TodoList extends React.Component {
   componentDidMount() {
     fetch("/api/todos")
       .then(res => res.json())
-      .then((result) => {this.setState({items: result});})
+      .then((result) => { this.setState({ items: result }); })
   }
 }
 
