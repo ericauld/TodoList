6f32f226576583e2c43373e937f4bbf5c02256b4
Eric Auld <eric.auld@gmail.com>
2020-12-03 13:56:52 -0800

    Create an input box on the frontend allowing user to add a new todo item
    The item is then added to both the database and the list on the GUI.
    This is via an input element inside TodoList.js. The input element is not
    yet inside a form; that is the next order of business.

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,12 +4,28 @@ import React from 'react';
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {items: []};
+    this.state = {items: [],
+                  inputBox: ''};
   }
   
+  createNewItem = (e) => {
+    if (e.key === 'Enter') {
+      var formData = new FormData()
+      formData.append('Title', this.state.inputBox)
+      fetch("/api/newItem", {method: "POST", body: formData})
+      this.setState((state, props) => ({
+        items: [...state.items, {"Title": this.state.inputBox}],
+        inputBox: ''
+      }))
+    }
+  }
+
+  handleChange = (e) => {this.setState({inputBox: e.target.value});}
+
   render() {
     return (
       <div>
+        <input onChange={this.handleChange} onKeyPress={this.createNewItem }/>
         <ul>
           {this.state.items.map(item => (
             <li>
