9f80e5ad220cd950b1db11f99cd7f7ccac20d4ca
Eric Auld <eric.auld@gmail.com>
2020-12-03 16:10:30 -0800

    Create a unit test for the delete item endpoint
    The test passes or fails according to the status of the response. Currently
    the status comes back as "404 Not Found", because the endpoint hasn't been created yet.

--- main_test.go
+++ main_test.go
@@ -5,13 +5,31 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	//"io/ioutil"
 	//"net/http/httptest"
-	//"encoding/json"
-	//"bytes"
+	"encoding/json"
+	"bytes"
 )
 
 func TestAddItem(t *testing.T) {
-	response, err := http.PostForm("http://localhost:8080/api/newItem", url.Values{"Title": {"new test item"}})
+	_, err := http.PostForm("http://localhost:8080/api/newItem", url.Values{"Title": {"new test item"}})
 	if err != nil {log.Fatal(err)}
-	_ = response
+}
+
+func TestDeleteItem(t *testing.T) {
+	client := http.Client{}
+
+	item := todoItem{"Go to store"}
+	jsonReq, err := json.Marshal(item)
+	request, err :=  http.NewRequest(
+		"DELETE",
+		"http://localhost:8080/api/deleteItem",
+		bytes.NewBuffer(jsonReq))
+	request.Header.Set("Content-Type", "application/json")
+	response, err := client.Do(request)
+	if err != nil {log.Fatal(err)}
+
+	if response.Status != "202 Accepted" {
+		log.Fatal("Response was not 202 Accepted; it was ", response.Status)
+	}
 }
