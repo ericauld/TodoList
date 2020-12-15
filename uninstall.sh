25f79bb476ffefa8b023a58169ac7dda96fe903f
ericauld <ericauld@ucla.edu>
2020-12-15 15:26:51 -0800

    Commit to be squashed Dec 15 3:27pm

--- uninstall.sh
+++ uninstall.sh
@@ -6,8 +6,14 @@ uninstall_mysql() {
     sudo rm -fr /etc/selinux/targeted/active/modules/100/mysql
     sudo rm -fr /usr/lib64/mysql
     sudo rm -fr /usr/share/mysql
+    sudo rm -fr ~/mysql*
     #change to if doesnt exist look at datadir variable in  my.cnf change all these to if existx
     #make it a list
 }
 
+uninstall_go() {
+    sudo rm -rvf /usr/local/go/
+}
+
 uninstall_mysql
+uninstall_go
