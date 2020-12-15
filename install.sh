a1e12efd17953566f1e8f2eec43223eb4c7d8fc6
ericauld <ericauld@ucla.edu>
2020-12-15 15:26:51 -0800

    Commit to be squashed Dec 15 3:27pm

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ testIfInstalledAndPromptForInstallation() {
         while true; do
             read -p "$command_name was not found. Would you like to install it? [y/N]" yn
             case $yn in
-            [Yy]*)
-                echo "Here I go! I'm installing some shit"
-                break
+                [Yy]*)
+                    echo "Here I go! I'm installing some shit"
+                    break
                 ;;
-            [Nn]*) return ;;
-            *) return ;;
+                [Nn]*) return ;;
+                *) return ;;
             esac
         done
     else
@@ -50,11 +50,25 @@ install_mysql() {
     if shellKnowsTheCommand apt-get; then
         apt-get update
         apt-get install mysql-shell
-    elif shellKnowsTheCommand; then
+        elif shellKnowsTheCommand wget; then
         sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
         sudo yum localinstall mysql57-community-release-el7-11.noarch.rpm
         sudo yum install mysql-community-server
         sudo systemctl start mysqld.service
+    else
+        echo Hi
+    fi
+}
+
+install_go() {
+    if shellKnowsTheCommand wget; then
+        sudo wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+        sudo tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
+        sudo export PATH=$PATH:/usr/local/go/bin
+        sudo source $HOME/.profile.
+        echo go installed with version $(go version)
+    else
+        echo "Could not install go because wget wasn't recognized"
     fi
 }
 
@@ -72,16 +86,34 @@ main() {
         while true; do
             read -p "$command_name was not found. Would you like to install it? [y/N]" yn
             case $yn in
-            [Yy]*)
-                install_mysql
-                break
+                [Yy]*)
+                    install_mysql
+                    break
                 ;;
-            [Nn]*) exit ;;
-            *) exit ;;
+                [Nn]*) exit ;;
+                *) exit ;;
             esac
         done
     else
-        echo Hi
+        echo I found $command_name on the system.
     fi
+    
+    command_name=go
+    if ! shellKnowsTheCommand $command_name; then
+        while true; do
+            read -p "$command_name was not found. Would you like to install it? [y/N]" yn
+            case $yn in
+                [Yy]*)
+                    install_go
+                    break
+                ;;
+                [Nn]*) exit ;;
+                *) exit ;;
+            esac
+        done
+    else
+        echo I found $command_name on the system.
+    fi
+    
 }
 main
