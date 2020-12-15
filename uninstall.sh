f93485dd36f9456ebe33d6405a1e8751b178d7ac
Eric Auld <ericauld@gmail.com>
2020-12-15 09:40:32 -0800

    Make scripts (un)install mysql
    Currently testing on Amazon Linux. Want to expand to other OSs, add error handling, refactor
    the script, (un)install go, clone the TodoList repository, etc.

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+uninstall_mysql() {
+    sudo yum remove mysql mysql-server
+    sudo rm -fr /var/lib/mysql
+    sudo rm -fr /etc/selinux/targeted/active/modules/100/mysql
+    sudo rm -fr /usr/lib64/mysql
+    sudo rm -fr /usr/share/mysql
+    #change to if doesnt exist look at datadir variable in  my.cnf change all these to if existx
+    #make it a list
+}
+
+uninstall_mysql
