aef8c7894d1ddeabc77513c7486c607379c232a6
Eric Auld <ericauld@gmail.com>
2020-12-15 09:40:32 -0800

    Make scripts (un)install mysql
    Currently testing on Amazon Linux. Want to expand to other OSs, add error handling, refactor
    the script, (un)install go, clone the TodoList repository, etc.

--- install.sh
+++ install.sh
@@ -14,49 +14,74 @@ testIfInstalledAndPromptForInstallation() {
             *) return ;;
             esac
         done
-        exit
     else
         echo "$command_name confirmed installed"
     fi
 }
 
-# vercomp is used from 
+# vercomp is used from
 # https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
-vercomp () {
-    if [[ $1 == $2 ]]
-    then
+vercomp() {
+    if [[ $1 == $2 ]]; then
         return 0
     fi
     local IFS=.
     local i ver1=($1) ver2=($2)
     # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
+    for ((i = ${#ver1[@]}; i < ${#ver2[@]}; i++)); do
         ver1[i]=0
     done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
+    for ((i = 0; i < ${#ver1[@]}; i++)); do
+        if [[ -z ${ver2[i]} ]]; then
             # fill empty fields in ver2 with zeros
             ver2[i]=0
         fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
-        then
+        if ((10#${ver1[i]} > 10#${ver2[i]})); then
             return 1
         fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
+        if ((10#${ver1[i]} < 10#${ver2[i]})); then
             return 2
         fi
     done
     return 0
 }
 
-main() {
-    testIfInstalledAndPromptForInstallation mysql
-    testIfInstalledAndPromptForInstallation yarn
-    testIfInstalledAndPromptForInstallation go
+install_mysql() {
+    if shellKnowsTheCommand apt-get; then
+        apt-get update
+        apt-get install mysql-shell
+    elif shellKnowsTheCommand; then
+        sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+        sudo yum localinstall mysql57-community-release-el7-11.noarch.rpm
+        sudo yum install mysql-community-server
+        sudo systemctl start mysqld.service
+    fi
 }
 
+shellKnowsTheCommand() {
+    command_name=$1
+    command -v $command_name &>/dev/null
+}
+
+main() {
+    # testIfInstalledAndPromptForInstallation yarn
+    # testIfInstalledAndPromptForInstallation mysql
+    # testIfInstalledAndPromptForInstallation go
+    command_name=mysql
+    if ! shellKnowsTheCommand $command_name; then
+        while true; do
+            read -p "$command_name was not found. Would you like to install it? [y/N]" yn
+            case $yn in
+            [Yy]*)
+                install_mysql
+                break
+                ;;
+            [Nn]*) exit ;;
+            *) exit ;;
+            esac
+        done
+    else
+        echo Hi
+    fi
+}
 main
